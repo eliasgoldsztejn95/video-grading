c76ee5efa65f910a46aa26fd31687f6f5ca8171a
Elias <75029654+eliasgoldsztejn95@users.noreply.github.com>
2024-09-06 14:31:00 +0300

    Update video_app.py

--- video_app.py
+++ video_app.py
@@ -137,7 +137,7 @@ if user_id:
             completed_videos_category = st.session_state.responses_df[(st.session_state.responses_df['user_id'] == user_id) &
                                                             (st.session_state.responses_df['category'] == category) &
                                                             (st.session_state.responses_df['completed'] == True)]['video'].tolist()
-            if all(video in completed_videos_category for video in videos):
+            if ln(completed_videos_category) == 4:
                 completed_categories.append(category)
     
         st.write("You have completed the following categories:")
